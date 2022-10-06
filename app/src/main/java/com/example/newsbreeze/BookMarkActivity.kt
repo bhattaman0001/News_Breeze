4f4f90797d88002c95bc2c3a109f5e97cf06650f
bhattaman0001 <bhattaman0001@gmail.com>
2022-10-06 16:29:24 +0530

    changes done

---
+++ app/src/main/java/com/example/newsbreeze/BookMarkActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newsbreeze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookMarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_mark)
+    }
+}
---
+++ app/src/main/java/com/example/newsbreeze/DetailedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newsbreeze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed)
+    }
+}
